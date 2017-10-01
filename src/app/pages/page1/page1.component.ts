793940c11cd4a56aa5817a99bd60c46c0503d066
Michel <michel_schevernels@hotmail.com>
2017-10-01 21:42:24 +0200

    contact backend

--- src/app/pages/page1/page1.component.ts
+++ src/app/pages/page1/page1.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../model/movie';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
+  providers: [MovieService]
 })
 export class Page1Component implements OnInit {
+  public movie : Movie;
 
-  constructor() { }
+  constructor(private _movieService:MovieService) {
+    
+   }
 
   ngOnInit() {
+    this._movieService.getMovie().subscribe(movie => {
+      this.movie = movie;
+    });
   }
 
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import { Movie } from '../model/movie';
 
 @Injectable()
 export class MovieService {
 
-  constructor() { }
+  private movie;
+  private url='http://localhost:8080/movies/1';
 
+  constructor(private _http : Http) { 
+   
+  }
+
+  getMovie() : Observable<Movie> {
+    return this._http.get(this.url)
+      .map(res=>res.json());
+  }
 }
